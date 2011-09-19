94dbdb45f28c14a06f6b35e0359705b36be80673
Angela Mao <amgirl326@gmail.com>
2011-09-19 14:04:10 -0400

    added meme's links

--- angela - healthcare/app/controllers/HomeController.js
+++ angela - healthcare/app/controllers/HomeController.js
@@ -75,4 +75,62 @@ Ext.regController('Home', {
         
         this.application.viewport.setActiveItem(this.recentView);
     },
+patientTypes: function()
+    {
+        if ( ! this.patientTypesView)
+        {
+            this.patientTypesView = this.render({
+                xtype: 'HomePatientTypes',
+            });
+        }
+     
+        var backBtn = this.application.viewport.query('#backBtn')[0];
+        backBtn.show();
+        
+        backBtn.setHandler(function()
+		{
+        	Ext.dispatch({
+        	    controller: 'Home',
+        	    action: 'index',
+        	    historyUrl: 'Home/index',
+        	    //
+        	    animation: {
+        	        type: 'slide',
+        	        reverse: true,
+        	    },
+        	});
+		});
+        
+        this.application.viewport.setActiveItem(this.patientTypesView);
+    },
+
+pregnantMothers: function()
+	    {
+		console.log("int function");
+	        if ( ! this.pregnantMothersView)
+	        {
+	            this.pregnantMothersView = this.render({
+	                xtype: 'HomePregnantMothers',
+	            });
+	        }
+
+	        var backBtn = this.application.viewport.query('#backBtn')[0];
+	        backBtn.show();
+
+	        backBtn.setHandler(function()
+			{
+	        	Ext.dispatch({
+	        	    controller: 'Home',
+	        	    action: 'patientTypes',
+	        	    historyUrl: 'Home/patientTypes',
+	        	    //
+	        	    animation: {
+	        	        type: 'slide',
+	        	        reverse: true,
+	        	    },
+	        	});
+			});
+
+	        this.application.viewport.setActiveItem(this.pregnantMothersView);
+	    },
 });
--- angela - healthcare/app/views/home/HomeIndexView.js
+++ angela - healthcare/app/views/home/HomeIndexView.js
@@ -1,5 +1,5 @@
 App.views.HomeIndex = Ext.extend(Ext.Panel, {
-	html: '<a href="#Home/about" class="menu-item">About</a> <a href="#Home/recent" class="menu-item">Recent</a>',
+	html: '<a href="#Home/about" class="menu-item">About</a> <a href="#Home/recent" class="menu-item">Recent</a> <a href="#Home/patientTypes" class="menu-item">Patient Types</a>',
 	scroll: 'vertical',
 	styleHtmlContent: true,
 	style: 'background: #d8e2ef',
