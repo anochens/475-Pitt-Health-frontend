4d90c3f213c8bb949eeb7b25cfcac03ec214d60b
Alan N <anochens@andrew.cmu.edu>
2011-09-24 13:11:09 -0400

    Removing angela folder (still accessible from v0.1)

--- angela - healthcare/app/views/home/HomePregnantMothersView.js
+++
@@ -1,8 +0,0 @@
-App.views.HomePregnantMothers = Ext.extend(Ext.Panel, {
-    	html: '<h2> Pregnant Mothers </h2>',
-		scroll: 'vertical',
-		styleHtmlContent: true,
-		style: 'background: #d8e2ef',
-	});
-	Ext.reg('HomePregnantMothers', App.views.HomePregnantMothers);
-	
