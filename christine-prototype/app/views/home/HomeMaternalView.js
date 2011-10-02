ec6a3ec9dbbb442a8e278239216f05f386c06944
Alan N <anochens@andrew.cmu.edu>
2011-10-02 01:17:38 -0400

    Misc cleanup

--- christine-prototype/app/views/home/HomeMaternalView.js
+++
@@ -1,8 +0,0 @@
-App.views.HomeMaternal = Ext.extend(Ext.Panel, {
-    html: '<h2>Maternal and Child Health</h2>',
-    scroll: 'vertical',
-    styleHtmlContent: true,
-    style: 'background: #d8efed'	
-});
-Ext.reg('HomeMaternal', App.views.HomeMaternal);
-
--- christine-prototype/app/views/home/HomeStopView.js
+++
@@ -1,8 +0,0 @@
-App.views.HomeStop = Ext.extend(Ext.Panel, {
-    html: '<h2>Pittsburgh STOP Program</h2>',
-    scroll: 'vertical',
-    styleHtmlContent: true,
-    style: 'background: #d8efed'	
-});
-Ext.reg('HomeStop', App.views.HomeStop);
-
