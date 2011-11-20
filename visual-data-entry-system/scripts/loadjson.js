d99efe522d0365ef2390b103886bdb9cb2a7b6a5
Ko Miyatake <ko.take5481@gmail.com>
2011-11-20 01:28:16 -0500

    sorted dropdown in alphabetical order, changed dialog box title and made the options radio buttons instead of a regular button

--- visual-data-entry-system/scripts/loadjson.js
+++ visual-data-entry-system/scripts/loadjson.js
@@ -57,5 +57,13 @@ function getItems(type){
 			items[window.jsondata[i].pk]=window.jsondata[i];
 		}
 	}
-	return items;
+	return items.sort(compare);
+}
+
+function compare(a,b) {
+  if (a.title < b.title)
+     return -1;
+  if (a.title > b.title)
+    return 1;
+  return 0;
 }
