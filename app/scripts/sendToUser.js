2128565747c47b3c1e4f410c37982a41c85b0d4c
Alan <anochens@andrew.cmu.edu>
2011-12-07 18:09:53 -0500

    Got rid of sms button

--- app/scripts/sendToUser.js
+++ app/scripts/sendToUser.js
@@ -2,14 +2,4 @@ function showMailerButton(){
 	var data = $("#main-data").html();
 	$('#email-button').show();
 	$('#email-button').attr("href", "mailto:?subject="+encodeURIComponent(document.title)+"&body=" + encodeURIComponent(data));
-}
-
-function showSMSButton(){
-	var data = $("#main-data").html();
-	var phone = data.match(/[01]?[- .]?\(?[2-9]\d{2}\)?[- .]?\d{3}[- .]?\d{4}/g);
-	$('#sms-button').show();
-	if (phone!= null){
-		$('#sms-button').attr("href", "sms:?body="+encodeURIComponent(document.title)+  encodeURIComponent(phone));
-	}
-
-}  
+} 
